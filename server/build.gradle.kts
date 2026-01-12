42488637a400288bab7ff7914d3d75c1bfca9b41
nageshdevadig-a <nageshdevadiga936@gmail.com>
2026-01-12 12:33:37 +0530

    Update MongoDB configuration and add validation dependency

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.projectlombok:lombok")
