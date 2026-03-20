20f70441b51c572c8b587adeb544b841e64fa9f1
nageshdevadig-a <nageshdevadiga936@gmail.com>
2026-03-20 20:04:09 +0530

    fix: update import path for SocketProvider to match new file naming convention

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
-import { SocketProvider } from './api/socketContext';
+import { SocketProvider } from './api/SocketContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
